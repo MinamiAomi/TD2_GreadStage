5e329b3e89f416a8a8ad454c2a04bfcc224e4c2f
hamu4792game <112112847+hamu4792game@users.noreply.github.com>
2023-11-13 22:23:51 +0900

    カメラ位置の調整

--- Game/CameraAnimation/CameraAnimation.cpp
+++ Game/CameraAnimation/CameraAnimation.cpp
@@ -11,7 +11,7 @@ void CameraAnimation::Initialize() {
 	transform_ = std::make_shared<Transform>();
 	//	X軸回転、俯瞰視点
 	transform_->rotate = Quaternion::MakeForXAxis(50.0f * Math::ToRadian);
-	transform_->translate = Vector3(0.0f, 20.0f, -30.0f);
+	transform_->translate = Vector3(0.0f, 0.0f, -30.0f);
 	offset_ = transform_->translate;
 
 }
@@ -21,10 +21,12 @@ void CameraAnimation::Update() {
 	if (target_) {
 		//transform_->translate.x = target_->worldMatrix.GetTranslate().x;
 	}
+	ImGui::Begin("camera");
 	ImGui::DragFloat3("Position", &transform_->translate.x, 0.01f);	
 	static Vector3 r = {0.0f, 0.0f,0.0f};
 	ImGui::DragFloat3("Rotate", &r.x, 0.1f, -360.0f, 360.0f);
 	transform_->rotate = Quaternion::MakeFromEulerAngle(r * Math::ToRadian);
+	ImGui::End();
 
 	camera_->SetPosition(transform_->translate);
 	camera_->SetRotate(transform_->rotate);
