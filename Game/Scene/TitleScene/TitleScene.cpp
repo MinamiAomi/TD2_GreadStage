3926a4785ef15b3945433a8ad5b924d481e9362e
hamu4792game <112112847+hamu4792game@users.noreply.github.com>
2023-11-16 12:53:01 +0900

    一時退避

--- Game/Scene/TitleScene/TitleScene.cpp
+++ Game/Scene/TitleScene/TitleScene.cpp
@@ -1,11 +1,41 @@
 #include "TitleScene.h"
 
-void TitleScene::OnInitialize() {
+#include "Scene/SceneManager.h"
+#include "Collision/CollisionManager.h"
+#include "Input/Input.h"
+#include "Scene/BattleScene/BattleScene.h"
 
+void TitleScene::OnInitialize() {
+	// 生成
+	player_ = std::make_shared<Player>();
+	camera_ = std::make_shared<CameraAnimation>();
+	stage_ = std::make_shared<Stage>();
+
+	// 初期化
+	player_->Initialize();
+	camera_->Initialize();
+	stage_->Initialize();
+
+	// セット
+	player_->SetCamera(camera_);
+	camera_->SetTarget(&player_->transform);
 }
 
 void TitleScene::OnUpdate() {
+	auto input = Input::GetInstance();
+	if (input->IsKeyPressed(DIK_0)) {
+		// シーンのシングルトンの取得
+		SceneManager* sceneManager = SceneManager::GetInstance();
+		// シーンの設定
+		sceneManager->ChangeScene<BattleScene>();
+	}
+	
+	stage_->Update();
+	player_->Update();
 
+	CollisionManager::GetInstance()->CheckCollision();
+	// カメラの更新
+	camera_->Update();
 }
 
 void TitleScene::OnFinalize() {
--- Game/Scene/TitleScene/TitleScene.h
+++ Game/Scene/TitleScene/TitleScene.h
@@ -2,7 +2,9 @@
 #include "Engine/Scene/BaseScene.h"
 #include <memory>
 
+#include "Game/Player/Player.h"
 #include "Game/CameraAnimation/CameraAnimation.h"
+#include "Game/Stage/Stage.h"
 
 class TitleScene : public BaseScene {
 public:
@@ -12,6 +14,8 @@ public:
 	void OnFinalize() override;
 
 private: // メンバ変数
+	std::shared_ptr<Player> player_;
 	std::shared_ptr<CameraAnimation> camera_;
+	std::shared_ptr<Stage> stage_;
 
 };
