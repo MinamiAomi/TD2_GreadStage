923ea0f51978e7327d1238a82268606500e5a641
hamu4792game <112112847+hamu4792game@users.noreply.github.com>
2023-11-22 15:37:41 +0900

    カメラの回転の実装

--- Game/CameraAnimation/CameraAnimation.cpp
+++ Game/CameraAnimation/CameraAnimation.cpp
@@ -2,6 +2,8 @@
 
 #include "Graphics/RenderManager.h"
 #include "Graphics/ImGuiManager.h"
+#include "Input/Input.h"
+#include <algorithm>
 
 void CameraAnimation::Initialize() {
 	SetName("Camera");
@@ -10,25 +12,56 @@ void CameraAnimation::Initialize() {
 	RenderManager::GetInstance()->SetCamera(camera_);
 
 	//	X軸回転、俯瞰視点
-	transform.translate = Vector3(0.0f, 10.0f, -10.0f);
+	transform.translate = Vector3(0.0f, 10.0f, -12.5f);
 	transform.rotate = Quaternion::MakeLookRotation(-transform.translate);
 	offset_ = transform.translate + Vector3{0.0f, -8.0f, 0.0f};
-
+	upDown.x = -10.0f;
+	upDown.y = 20.0f;
 }
 
 void CameraAnimation::Update() {
 	
+
+	DrawImGui();
+
 	if (target_) {
 		//transform_->translate.x = target_->worldMatrix.GetTranslate().x;
-		transform.rotate = target_->rotate;
+		auto input = Input::GetInstance();
+		auto& xinput = input->GetXInputState();
+		if (input->IsKeyPressed(DIK_RIGHTARROW)
+			|| xinput.Gamepad.sThumbRX > XINPUT_GAMEPAD_RIGHT_THUMB_DEADZONE) {
+			radRot_.y -= 1.0f;
+		}
+		if (input->IsKeyPressed(DIK_LEFTARROW)
+			|| xinput.Gamepad.sThumbRX < -XINPUT_GAMEPAD_RIGHT_THUMB_DEADZONE) {
+			radRot_.y += 1.0f;
+		}
+		if (input->IsKeyPressed(DIK_DOWNARROW)
+			|| xinput.Gamepad.sThumbRY < -XINPUT_GAMEPAD_RIGHT_THUMB_DEADZONE) {
+			radRot_.x -= 1.0f;
+		}
+		if (input->IsKeyPressed(DIK_UPARROW)
+			|| xinput.Gamepad.sThumbRY > XINPUT_GAMEPAD_RIGHT_THUMB_DEADZONE) {
+			radRot_.x += 1.0f;
+		}
+		
+		
+		// 制限
+		if (std::abs(radRot_.y) >= 360.0f) {
+			radRot_.y = 0.0f;
+		}
+		radRot_.x = std::clamp(radRot_.x, upDown.x, upDown.y);
+
+		// Y軸回転
+		transform.rotate = Quaternion::MakeFromAngleAxis(radRot_.y * Math::ToRadian, target_->rotate.GetUp());
+		// X軸回転
+		Quaternion r = Quaternion::MakeFromAngleAxis(radRot_.x * Math::ToRadian, target_->rotate.GetRight());
+		transform.rotate *= r;
+		transform.rotate *= target_->rotate;
 		transform.translate = target_->translate + (transform.rotate * offset_);
 	}
-	//ImGui::Begin("camera");
-	//ImGui::DragFloat3("Position", &transform_->translate.x, 0.01f);	
-	//static Vector3 r = {0.0f, 0.0f,0.0f};
-	//ImGui::DragFloat3("Rotate", &r.x, 0.1f, -360.0f, 360.0f);
-	//transform_->rotate = Quaternion::MakeFromEulerAngle(r * Math::ToRadian);
-	//ImGui::End();
+	
+
 	transform.UpdateMatrix();
 	camera_->SetPosition(transform.translate);
 	camera_->SetRotate(transform.rotate);
@@ -38,4 +71,14 @@ void CameraAnimation::Update() {
 
 void CameraAnimation::Restart() {
 
+}
+
+void CameraAnimation::DrawImGui() {
+#ifdef _DEBUG
+	ImGui::Begin("camera");
+	ImGui::DragFloat3("Position", &offset_.x, 0.01f);
+	ImGui::DragFloat3("radR", &radRot_.x, 0.01f);
+	ImGui::DragFloat2("Limit Down:Up", &upDown.x, 0.01f);
+	ImGui::End();
+#endif // _DEBUG
 }
