ba26387715ef6390eb233c75c8965f5961087ac8
hamu4792game <112112847+hamu4792game@users.noreply.github.com>
2023-11-27 02:35:30 +0900

    battleSceneに何もなければStage1をロードするよう修正

--- Game/Transition/Transition.cpp
+++ Game/Transition/Transition.cpp
@@ -7,11 +7,14 @@ Transition* Transition::GetInstance() {
 }
 
 void Transition::Initialize() {
+	number = 1;
+	isTransition_ = false;
+
 	texture_ = std::make_unique<Sprite>();
 	texture_->SetTexture(ResourceManager::GetInstance()->FindTexture("Block"));
 	texture_->SetIsActive(true);
 	texture_->SetDrawOrder(1);
-	texture_->SetScale(Vector2(1280.0f, 720.0f));
+	texture_->SetScale(Vector2(10.0f, 10.0f));
 	texture_->SetPosition(Vector2(640.0f, 360.0f));
 	texture_->SetColor(Vector4(1.0f, 1.0f, 1.0f, 1.0f));
 }
