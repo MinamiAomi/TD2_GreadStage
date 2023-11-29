3960285c70a6e2dd72d5ca73c60892c55febd1f1
MinamiAomi <k022g0040@g.neec.ac.jp>
2023-11-30 06:25:05 +0900

    sss

--- Game/MasterGame/MasterGame.cpp
+++ Game/MasterGame/MasterGame.cpp
@@ -73,16 +73,14 @@ void MasterGame::OnInitialize() {
     LoadModel("StageSelect", "Resources/Model/Stage/stageSelect.obj", true);
     // Stage1
     LoadModel("Stage1", "Resources/Model/Stage/stage1.obj", true);
-    //// Stage2
-    //LoadModel("Stage2", "Resources/Model/Stage/stage2.obj");
-    //// Stage3
-    //LoadModel("Stage3", "Resources/Model/Stage/stage3.obj");
-    //// Stage4
-    //LoadModel("Stage4", "Resources/Model/Stage/stage4.obj");
-    //// Stage5
-    //LoadModel("Stage5", "Resources/Model/Stage/stage5.obj");
-    //// Stage6
-    //LoadModel("Stage6", "Resources/Model/Stage/stage6.obj");
+    // Stage2
+    LoadModel("Stage2", "Resources/Model/Stage/stage2.obj", true);
+    // Stage3
+    LoadModel("Stage3", "Resources/Model/Stage/stage3.obj", true);
+    // Stage4
+    LoadModel("Stage4", "Resources/Model/Stage/stage4.obj", true);
+    // Stage5
+    LoadModel("Stage5", "Resources/Model/Stage/stage5.obj", true);
 
     // ブロック
     LoadTexture("Block", "Resources/Texture/block.png");
